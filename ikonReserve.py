882b019fae7068e0aa834e3bc0c1dfc7166bcabe
ashkevin22 <65938312+ashkevin22@users.noreply.github.com>
2021-01-09 00:59:43 -0700

    Fixing CSS paths and input validation

--- ikonReserve.py
+++ ikonReserve.py
@@ -10,16 +10,24 @@ numToMonth = dict(zip(monthsNum, months))
 def dateInput():
     validate = True
     while validate:
-        dateMonth = int(input("Enter the month: "))
-        if (dateMonth <= 6) and (dateMonth >= 1):
+        rawInput = input("Enter the desired month (1-6): ")
+        if rawInput.isdigit():
+            dateMonth = int(rawInput)
+        if not rawInput.isdigit():
+            print("Please enter a number")
+        elif (dateMonth <= 6) and (dateMonth >= 1):
             validate = False
         else:
-            print("Enter a valid month (1-6)")
+            print("Please enter a valid month")
 
     validate = True
     while validate:
-        dateDay = int(input("Enter the day: "))
-        if dateMonth in [1, 3, 5, 7, 8, 10, 12]:
+        rawInput = input("Enter the desired day: ")
+        if rawInput.isdigit():
+            dateDay = int(rawInput)
+        if not rawInput.isdigit():
+            print("Please enter a number")
+        elif dateMonth in [1, 3, 5, 7, 8, 10, 12]:
             if (dateDay <= 31) and (dateDay >= 1):
                 validate = False
             else:
@@ -45,10 +53,10 @@ def fromReserve(listDate, inputBool):
     button = driver.find_element_by_css_selector("#react-autowhatever-resort-picker-section-0-item-3 > span > span")
     button.click()
     time.sleep(2)
-    button = driver.find_element_by_css_selector("#root > div > div > main > section.sc-fzqyOu.ijDjOa > div > div.amp-card.sc-prRJy.gqNvjh > div.sc-qQjAt.kHbQih > div.sc-qQLmQ.eyRWNm > button")
+    button = driver.find_element_by_css_selector("#root > div > div > main > section.sc-pBolk.bLdbNO > div > div.amp-card.sc-pRrxg.OYQvJ > div.sc-pZOOJ.iFmuIW > div.sc-pIuOK.kfwIKW > button")
     button.click()
     time.sleep(2)
-    elem = driver.find_element_by_css_selector("#root > div > div > main > section.sc-fzqyOu.ijDjOa > div > div.amp-card.sc-prRJy.gqNvjh > div.sc-qQjAt.kHbQih > div.sc-pByoR.YJVtY > div:nth-child(1) > div.DayPicker.sc-qPmLO.gspUft > div > div.sc-pRrUz.eJFkCj > div.sc-oTPjJ.iiFRwl > span")
+    elem = driver.find_element_by_css_selector("#root > div > div > main > section.sc-pBolk.bLdbNO > div > div.amp-card.sc-pRrxg.OYQvJ > div.sc-pZOOJ.iFmuIW > div.sc-pZpxQ.kynict > div:nth-child(1) > div.DayPicker.sc-pIVsU.fBycfn > div > div.sc-pcHDm.cSajLG > div.sc-ptCms.fzKffT > span")
 
     returnVal = 0
 
@@ -59,7 +67,7 @@ def fromReserve(listDate, inputBool):
             returnVal = checkAvailable(listDate, inputBool)
             findMonth = False
         else:
-            button = driver.find_element_by_css_selector("#root > div > div > main > section.sc-fzqyOu.ijDjOa > div > div.amp-card.sc-prRJy.gqNvjh > div.sc-qQjAt.kHbQih > div.sc-pByoR.YJVtY > div:nth-child(1) > div.DayPicker.sc-qPmLO.gspUft > div > div.sc-pRrUz.eJFkCj > div.sc-oTPjJ.iiFRwl > button:nth-child(3)")
+            button = driver.find_element_by_css_selector("#root > div > div > main > section.sc-pBolk.bLdbNO > div > div.amp-card.sc-pRrxg.OYQvJ > div.sc-pZOOJ.iFmuIW > div.sc-pZpxQ.kynict > div:nth-child(1) > div.DayPicker.sc-pIVsU.fBycfn > div > div.sc-pcHDm.cSajLG > div.sc-ptCms.fzKffT > button:nth-child(3)")
             button.click()
     return returnVal
 
@@ -86,7 +94,7 @@ def checkAvailable(listDate, inputBool):
         for x in elems:
             if str(day) == x.text.strip():
                 x.click()
-        reservationsLeft = driver.find_element_by_css_selector("#root > div > div > main > section.sc-fzqyOu.ijDjOa > div > div.amp-card.sc-prRJy.gqNvjh > div.sc-qQjAt.kHbQih > div.sc-pByoR.YJVtY > div:nth-child(2) > div > div.sc-pIkGh.gXAuWj > label > div.sc-pQUwV.hGUBBX > div.sc-ptCms.iXMYHo")
+        reservationsLeft = driver.find_element_by_css_selector("#root > div > div > main > section.sc-pBolk.bLdbNO > div > div.amp-card.sc-pRrxg.OYQvJ > div.sc-pZOOJ.iFmuIW > div.sc-pZpxQ.kynict > div:nth-child(2) > div > div.sc-oVfmS.fqKNoT > label > div.sc-pdihw.bqUhYY > div.sc-pCOsa.qskIT")
         numReservations = ""
         for c in reservationsLeft.text:
             if c.isdigit():
@@ -95,15 +103,15 @@ def checkAvailable(listDate, inputBool):
             print("Could not book reservation, you have 0 days left.")
             return
         else:
-            button = driver.find_element_by_css_selector("#root > div > div > main > section.sc-fzqyOu.ijDjOa > div > div.amp-card.sc-prRJy.gqNvjh > div.sc-qQjAt.kHbQih > div.sc-pByoR.YJVtY > div:nth-child(2) > div > div.sc-pCOsa.eakatQ > button.sc-AxjAm.jxPclZ.sc-pAArZ.lkoEyq")
+            button = driver.find_element_by_css_selector("#root > div > div > main > section.sc-pBolk.bLdbNO > div > div.amp-card.sc-pRrxg.OYQvJ > div.sc-pZOOJ.iFmuIW > div.sc-pZpxQ.kynict > div:nth-child(2) > div > div.sc-pZAOG.eUgRHN > button.sc-AxjAm.jxPclZ.sc-qcpLw.jSQblL")
             button.click()
             time.sleep(1)
-            button = driver.find_element_by_css_selector("#root > div > div > main > section.sc-fzqyOu.ijDjOa > div > div.amp-card.sc-prRJy.gqNvjh > div.sc-qQjAt.kHbQih > div.sc-qWPci.fYvXyK > button")
+            button = driver.find_element_by_css_selector("#root > div > div > main > section.sc-pBolk.bLdbNO > div > div.amp-card.sc-pRrxg.OYQvJ > div.sc-pZOOJ.iFmuIW > div.sc-oTAMn.eTrOuH > button")
             button.click()
             time.sleep(2)
-            button = driver.find_element_by_css_selector("#root > div > div > main > section.sc-fzqyOu.ijDjOa > div > div.amp-card.sc-prRJy.gqNvjh > div.sc-qQjAt.kHbQih > div > div.sc-pAMbm.bWxMRp > label > input")
+            button = driver.find_element_by_css_selector("#root > div > div > main > section.sc-pBolk.bLdbNO > div > div.amp-card.sc-pRrxg.OYQvJ > div.sc-pZOOJ.iFmuIW > div > div.sc-pYZcj.ffynVg > label > input")
             button.click()
-            button = driver.find_element_by_css_selector("#root > div > div > main > section.sc-fzqyOu.ijDjOa > div > div.amp-card.sc-prRJy.gqNvjh > div.sc-qQjAt.kHbQih > div > div.sc-qQkIG.edTdYL > button")
+            button = driver.find_element_by_css_selector("#root > div > div > main > section.sc-pBolk.bLdbNO > div > div.amp-card.sc-pRrxg.OYQvJ > div.sc-pZOOJ.iFmuIW > div > div.sc-pJTpM.ieaJIt > button")
             button.click()
             print("Successfully booked!")
             return 1
