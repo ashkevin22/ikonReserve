7610ef284b495e69d022f69615a25fa3fc8fc414
ashkevin22 <65938312+ashkevin22@users.noreply.github.com>
2021-01-09 02:11:52 -0700

    allow user to choose resort
    Still only able to do resorts through the ikon pass website, not parking reservations

--- ikonReserve.py
+++ ikonReserve.py
@@ -1,4 +1,4 @@
-import tkinter
+import tkinter as tk
 from selenium import webdriver
 import time
 
@@ -46,11 +46,15 @@ def dateInput():
     return [numToMonth[dateMonth], dateDay]
 
 
-def fromReserve(listDate, inputBool):
+def fromReserve(listDate, inputBool, section, item):
     stringMonth = listDate[0]
     day = listDate[1]
-    time.sleep(2)
-    button = driver.find_element_by_css_selector("#react-autowhatever-resort-picker-section-0-item-3 > span > span")
+    driver.execute_script("window.scrollTo(0, 200)")
+    elems = driver.find_elements_by_class_name("react-autosuggest__section-title")
+    for x in elems:
+        if x.text.strip().lower() == "my favorites":
+            section += 1
+    button = driver.find_element_by_css_selector("#react-autowhatever-resort-picker-section-" + str(section) + "-item-" + str(item) + " > span > span")
     button.click()
     time.sleep(2)
     button = driver.find_element_by_css_selector("#root > div > div > main > section.sc-pBolk.bLdbNO > div > div.amp-card.sc-pRrxg.OYQvJ > div.sc-pZOOJ.iFmuIW > div.sc-pIuOK.kfwIKW > button")
@@ -100,7 +104,7 @@ def checkAvailable(listDate, inputBool):
             if c.isdigit():
                 numReservations += c
         if int(numReservations) == 0:
-            print("Could not book reservation, you have 0 days left.")
+            print("Could not book reservation. You have 0 remaining reservations.")
             return
         else:
             button = driver.find_element_by_css_selector("#root > div > div > main > section.sc-pBolk.bLdbNO > div > div.amp-card.sc-pRrxg.OYQvJ > div.sc-pZOOJ.iFmuIW > div.sc-pZpxQ.kynict > div:nth-child(2) > div > div.sc-pZAOG.eUgRHN > button.sc-AxjAm.jxPclZ.sc-qcpLw.jSQblL")
@@ -116,6 +120,86 @@ def checkAvailable(listDate, inputBool):
             print("Successfully booked!")
             return 1
 
+root = tk.Tk()
+
+v = tk.IntVar()
+v.set(10)  # initializing the choice, i.e. Python
+
+languages = [("Alta Snowbird", 0),("Arapahoe Basin", 1),("Big Sky", 2),("Brighton", 3),("Copper Mountain", 4),("Deer Valley", 5),("Eldora", 6),("Solitude", 7),("Steamboat", 8),("Taos", 9),("Winter Park", 10),("Big Bear", 11),("June Mountain", 11),("Mammoth", 13),("Squaw Valley", 14),("Boyne Highlands", 15),("Boyne Mountain", 16),("Crystal Mountain", 17),("Mt. Bachelor", 18),("Summit at Snoqualmie", 19),("Killington", 20),("Loon Mountain",21),("Snowshoe",22),("Stratton",23),("Sugarbush",24),("Sugarloaf",25),("Sunday River",26),("Windham Mountain",27),("Blue Mountain",28),("Tremblant",29),("Cypress Mountain",30),("Red Mountain",31),("Revelstoke",32),("Ski Big 3",33),("Coronet Peak",34),("Mt. Butler",35),("Thredbo",36),("Niseko United",37),("Valle Nevado",38),("Zermatt",39)]
+
+
+def submit():
+    root.destroy()
+
+
+tk.Label(root, text="Choose the desired destination",justify = 'center',).grid(column=1, row=0, columnspan=2)
+
+for i, langVal in enumerate(languages):
+    if i > 19:
+        col = 2
+        rowNum = i-19
+    else:
+        col = 1
+        rowNum = i+1
+    tk.Radiobutton(root, text=langVal[0], padx = 20, variable=v, value=langVal[1]).grid(column=col, row=rowNum, sticky="W")
+tk.Button(text="Submit", command=submit).grid(column=1, row=21, columnspan=2)
+
+root.attributes('-alpha',0)
+root.update()
+
+# Gets the requested values of the height and width.
+windowWidth = root.winfo_reqwidth()
+windowHeight = root.winfo_reqheight()
+
+# Gets both half the screen width/height and window width/height
+positionRight = int(root.winfo_screenwidth() / 2 - windowWidth / 2)
+positionDown = int(root.winfo_screenheight() / 2 - windowHeight / 2)
+
+# Positions the window in the center of the page.
+root.geometry("+{}+{}".format(positionRight, positionDown))
+
+root.attributes('-alpha',1)
+root.mainloop()
+
+location = int(v.get())
+
+if location <= 10:
+    section = 0
+    item = location
+elif location <= 14:
+    section = 1
+    item = location-11
+elif location <= 16:
+    section = 2
+    item = location-15
+elif location <= 19:
+    section = 3
+    item = location-17
+elif location <= 27:
+    section = 4
+    item = location-20
+elif location <= 29:
+    section = 5
+    item = location-28
+elif location <= 33:
+    section = 6
+    item = location-30
+elif location == 34:
+    section = 7
+    item = 0
+elif location <= 36:
+    section = 8
+    item = location-35
+elif location == 37:
+    section = 9
+    item = 0
+elif location == 38:
+    section = 10
+    item = 0
+elif location == 29:
+    section = 11
+    item = 0
+
 driver = webdriver.Chrome()
 driver.get("https://account.ikonpass.com/en/myaccount/add-reservations/")
 time.sleep(2)
@@ -125,6 +209,6 @@ while driver.current_url != "https://account.ikonpass.com/en/myaccount":
 
 driver.get("https://account.ikonpass.com/en/myaccount/add-reservations/")
 
-fromReserve(dateInput(), True)
+fromReserve(dateInput(), True, section, item)
 
 driver.close()
